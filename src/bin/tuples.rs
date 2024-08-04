3e4afbb1ca961266e129d84bf99fa7f2e71681d4
R Sriram <rinnegan.sr@gmail.com>
2024-08-04 21:14:25 +0530

    finished lesson

--- src/bin/tuples.rs
+++ src/bin/tuples.rs
@@ -1,7 +1,15 @@
+fn reverse (pair: (&str,bool)) -> (bool,&str)
+{
+    let (string_param, bool_param) = pair;
+    (bool_param, string_param)
+}
 fn main()
 {
+    let friend = ("luffy",true);
     let long_tuple = (12, 2.5, "batman", 1969, false);
     println!("tuple value number 0: {}", long_tuple.0);
     let tupple_ception = (("metropolis", false, 12.4),("goku",true,'g'));
     println!("tupple in a tupple {:?}", tupple_ception);
+    println!("regular tupple: {:?}", friend);
+    println!("reversed tupple: {:?}", reverse(friend));
 }
